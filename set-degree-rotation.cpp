7e511cd50efdcd9a92da7d4f116f0ec3e7ce7a38
TheLostProgrammer <77237673+TheLostProgrammer@users.noreply.github.com>
2022-10-10 15:38:05 +1030

    Create set-degree-rotation.cpp
    Code created regarding a comment about restricting the stepper motor to stop at 90˚ and turn back to stop at -90˚.

---
+++ set-degree-rotation.cpp
@@ -0,0 +1,23 @@
+#include <Stepper.h>
+int button_one = 13;
+int button_two = 12;
+float steps = 2048;
+float step = 360 / steps;
+float current_degree;
+float stop_degrees = 90;
+Stepper stepper(steps, 7, 9, 8, 10);
+
+void setup() {
+  pinMode(button_one, INPUT);
+  pinMode(button_two, INPUT);
+  stepper.setSpeed(10);
+}
+
+void loop() {
+  if (digitalRead(button_one) & current_degree != stop_degrees) {
+    stepper.step(1);
+    current_degree += step;
+  } else if (digitalRead(button_two) & current_degree != -stop_degrees) {
+    stepper.step(-1);
+    current_degree -= step;
+  }
